6bbe4bda13984e6f22233498ce1d70f08f11d3fa
Humra <matija.huremovic@gmail.com>
2018-11-22 14:46:47 +0100

    Base stats and armour/damage modifiers

---
+++ Archescape/Assets/Scripts/Stats/CharacterStats.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class CharacterStats : MonoBehaviour {
+
+    public int maxHealth = 10;
+    public int currentHealth
+    {
+        get;
+        private set;
+    }
+    public Stat damage;
+    public Stat armour;
+
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            TakeDamage(10);
+        }
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        damageAmount -= armour.GetValue();
+        damageAmount = Mathf.Clamp(damageAmount, 0, int.MaxValue);
+
+        currentHealth -= damageAmount;
+        Debug.Log(transform.name + " takes " + damageAmount + " damage.");
+
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public virtual void Die()
+    {
+        Debug.Log(transform.name + " died");
+    }
+}
---
+++ Archescape/Assets/Scripts/Stats/PlayerStats.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : CharacterStats {
+
+    private void Start()
+    {
+        EquipmentManager.instance.onEquipmentChanged += OnEquipmentChanged;
+    }
+
+    private void OnEquipmentChanged(Equipment newItem, Equipment oldItem)
+    {
+        Debug.Log("OnItemChanged in PlayerStats");
+        if(newItem != null)
+        {
+            armour.AddModifier(newItem.armourModifier);
+            damage.AddModifier(newItem.damageModifier);
+        }
+        
+        if(oldItem != null)
+        {
+            armour.RemoveModifier(oldItem.armourModifier);
+            damage.RemoveModifier(oldItem.damageModifier);
+        }
+    }
+}
