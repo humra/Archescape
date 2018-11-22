0425d3b627265d93889816086519f2b8e90ebe9c
Humra <matija.huremovic@gmail.com>
2018-11-22 15:05:47 +0100

    Basic enemy AI

--- Archescape/Assets/Scripts/GameManager.cs
+++ Archescape/Assets/Scripts/GameManager.cs
@@ -40,12 +40,10 @@ public class GameManager : MonoBehaviour {
     {
         if(walkableMask == (walkableMask | 1 << hit.collider.gameObject.layer))
         {
-            Debug.Log("Move");
             player.MoveToPoint(hit.point);
         }
         else if(interactibleMask == (interactibleMask | 1 << hit.collider.gameObject.layer))
         {
-            Debug.Log("Interact");
             Interactible interactible = hit.collider.gameObject.GetComponent<Interactible>();
             player.MoveToPoint(interactible.interactionTransform.position, interactible.radius);
             interactible.Interact();
