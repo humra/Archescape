f0d6a71c264391ba5db5475763de18f1b1119c6e
Humra <matija.huremovic@gmail.com>
2018-11-28 14:03:31 +0100

    Cleanup of unused code

---
+++ Archescape/Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAnimator : MonoBehaviour {
+
+    protected Animator animator;
+    private NavMeshAgent agent;
+
+    [SerializeField]
+    private float dampTime = 0.1f;
+
+	protected virtual void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+	}
+	
+	protected virtual void Update () {
+        float speedPercent = agent.velocity.magnitude / agent.speed;
+        animator.SetFloat("speedPercent", speedPercent, dampTime, Time.deltaTime);
+	}
+}
